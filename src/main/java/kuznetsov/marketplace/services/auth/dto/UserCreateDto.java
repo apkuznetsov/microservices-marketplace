ebc3f1b6db67aebebe706525b322df0e14b33b18
Andrey <ethenaut@ya.ru>
2022-11-20 15:52:36 +0400

    def user create dto

---
+++ src/main/java/kuznetsov/marketplace/services/auth/dto/UserCreateDto.java
@@ -0,0 +1,21 @@
+package kuznetsov.marketplace.services.auth.dto;
+
+import kuznetsov.marketplace.domain.user.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserCreateDto {
+
+  private String email;
+
+  private String password;
+
+  private UserRole role;
+
+}
