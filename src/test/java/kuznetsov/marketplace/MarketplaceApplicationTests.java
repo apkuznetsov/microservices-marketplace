8ff381b9f10543e3fb57c6076fb8e5cc81a126de
Andrey <ethenaut@ya.ru>
2022-11-20 12:11:52 +0400

    create template project

---
+++ src/test/java/kuznetsov/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package kuznetsov.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
