c3c505d89b3cc45747503c6e8996de4d23bd487d
Andrey <ethenaut@ya.ru>
2022-11-21 18:01:27 +0400

    def and impl register customer

--- src/main/java/kuznetsov/marketplace/services/user/UserService.java
+++ src/main/java/kuznetsov/marketplace/services/user/UserService.java
@@ -4,6 +4,8 @@ import kuznetsov.marketplace.services.user.dto.UserDto;
 
 public interface UserService {
 
+  UserDto registerCustomer(String email, String password);
+
   UserDto getUserByEmail(String email);
 
 }
