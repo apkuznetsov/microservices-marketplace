50762f4563a0e08d9909d79824db9ad502ff3707
Andrey <ethenaut@ya.ru>
2022-11-21 18:00:22 +0400

    def user already exists error code

--- src/main/java/kuznetsov/marketplace/services/user/exception/UserErrorCode.java
+++ src/main/java/kuznetsov/marketplace/services/user/exception/UserErrorCode.java
@@ -12,7 +12,12 @@ public enum UserErrorCode implements ServiceErrorCode {
   USER_NOT_FOUND(
       "User Not Found",
       "The specified user is not found in the system.",
-      HttpStatus.NOT_FOUND);
+      HttpStatus.NOT_FOUND),
+
+  USER_ALREADY_EXISTS(
+      "User Already Exists",
+      "The specified user already exists in the system.",
+      HttpStatus.BAD_REQUEST);
 
   private final String name;
   private final String message;
