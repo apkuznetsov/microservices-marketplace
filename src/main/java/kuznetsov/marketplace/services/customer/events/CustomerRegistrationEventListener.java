9b19e55462efc7e99f500d2d142031743bfde321
Andrey <ethenaut@ya.ru>
2022-11-22 11:24:44 +0400

    def customer registration event listener

---
+++ src/main/java/kuznetsov/marketplace/services/customer/events/CustomerRegistrationEventListener.java
@@ -0,0 +1,16 @@
+package kuznetsov.marketplace.services.customer.events;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomerRegistrationEventListener {
+
+  @EventListener
+  public void handleCustomerRegistration(CustomerRegistrationEvent event) {
+    log.info("Registration event got triggered for customer with {} email.", event.getEmail());
+  }
+
+}
