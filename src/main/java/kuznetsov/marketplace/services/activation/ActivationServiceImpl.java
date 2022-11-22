adddd451c752530ba535f5fe03990a703136d522
Andrey <ethenaut@ya.ru>
2022-11-22 23:26:21 +0400

    def and use activation properties

--- src/main/java/kuznetsov/marketplace/services/activation/ActivationServiceImpl.java
+++ src/main/java/kuznetsov/marketplace/services/activation/ActivationServiceImpl.java
@@ -3,21 +3,20 @@ package kuznetsov.marketplace.services.activation;
 import kuznetsov.marketplace.services.activation.dto.ActivationRequest;
 import kuznetsov.marketplace.services.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ActivationServiceImpl implements ActivationService {
 
+  private final ActivationProperties activationProps;
+
   private final JwtService jwtService;
 
-  @Value("${activation.url}")
-  private String activationUrl;
 
   @Override
   public String getActivationUrl() {
-    return activationUrl;
+    return activationProps.getUrl();
   }
 
   @Override
