474788485f6af0f9af14981c36ae081fa615b43a
Andrey <ethenaut@ya.ru>
2022-11-22 13:22:50 +0400

    permit all requests

--- src/main/java/kuznetsov/marketplace/web/configuration/SecurityConfiguration.java
+++ src/main/java/kuznetsov/marketplace/web/configuration/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package kuznetsov.marketplace.web.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +15,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Bean
   public AuthenticationManager beanAuthenticationManager() throws Exception {
     return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors()
+        .and()
+        .csrf().disable()
+        .formLogin().disable()
+        .httpBasic().disable()
+        .authorizeRequests()
+        .antMatchers("/**").permitAll()
+        .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 
 }
