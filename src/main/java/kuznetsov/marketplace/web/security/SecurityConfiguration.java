13dcc089cdb95e8b837794c953a18a87cc227b73
Andrey <ethenaut@ya.ru>
2022-11-23 12:54:17 +0400

    move security config to security package

--- src/main/java/kuznetsov/marketplace/web/security/SecurityConfiguration.java
+++ src/main/java/kuznetsov/marketplace/web/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package kuznetsov.marketplace.web;
+package kuznetsov.marketplace.web.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
