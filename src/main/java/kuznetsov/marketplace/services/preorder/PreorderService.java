5a231f57a4910c42ba880716e728b6973b78f65e
Andrey <ethenaut@ya.ru>
2022-12-05 20:49:09 +0400

    def preorder service

---
+++ src/main/java/kuznetsov/marketplace/services/preorder/PreorderService.java
@@ -0,0 +1,23 @@
+package kuznetsov.marketplace.services.preorder;
+
+import kuznetsov.marketplace.services.preorder.dto.PreorderCreateDto;
+import kuznetsov.marketplace.services.preorder.dto.PreorderDto;
+import kuznetsov.marketplace.services.preorder.dto.PreorderDtoPage;
+import kuznetsov.marketplace.services.preorder.dto.PreorderParticipationsDtoPage;
+
+public interface PreorderService {
+
+  PreorderDto addSellerPreorder(String sellerEmail, PreorderCreateDto preorderDto);
+
+  PreorderDto getPreorderByIdWithCurrentUserParticipationStatus(String userEmail, long preorderId);
+
+  PreorderDtoPage getPagedSellerPreorders(String sellerEmail, int pageNum);
+
+  PreorderDtoPage getPagedCustomerPreorders(String customerEmail, int pageNum);
+
+  void participateCustomerInPreorderByPreorderId(String customerEmail, long preorderId);
+
+  PreorderParticipationsDtoPage getSellerPreorderParticipationsPageByPreorderId(
+      long preorderId, String currentSellerEmail, int pageNum);
+
+}
