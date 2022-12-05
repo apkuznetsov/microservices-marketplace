aceab4b9202fe60e669cdc98650afdf034e7c5db
Andrey <ethenaut@ya.ru>
2022-12-05 22:17:31 +0400

    def customer

---
+++ src/main/java/kuznetsov/marketplace/domain/user/Customer.java
@@ -0,0 +1,46 @@
+package kuznetsov.marketplace.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "customer")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, unique = true)
+  private Long id;
+
+  @Column(name = "name", nullable = false, length = 255)
+  private String name;
+
+  @Column(name = "address", nullable = false, length = 1000)
+  private String address;
+
+  @Column(name = "country", nullable = false, length = 255)
+  private String country;
+
+  @Column(name = "public_email", nullable = false, length = 255)
+  private String publicEmail;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "c_user_id", nullable = false)
+  private User user;
+
+}
