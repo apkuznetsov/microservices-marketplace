3e0a11b16d5ececc63fff7588396fe18b0607435
Andrey <ethenaut@ya.ru>
2022-12-05 10:16:38 +0400

    def product seller dto

---
+++ src/main/java/kuznetsov/marketplace/services/product/dto/ProductSellerDto.java
@@ -0,0 +1,20 @@
+package kuznetsov.marketplace.services.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductSellerDto {
+
+  private String name;
+
+  private String address;
+
+  private String email;
+
+}
