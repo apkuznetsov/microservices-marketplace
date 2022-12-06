760d784ed49a06282ac13b0a4dc488c5baa0f6e0
Andrey <ethenaut@ya.ru>
2022-12-06 21:04:12 +0400

    def product repo queries

--- src/main/java/kuznetsov/marketplace/database/product/ProductRepository.java
+++ src/main/java/kuznetsov/marketplace/database/product/ProductRepository.java
@@ -1,10 +1,25 @@
 package kuznetsov.marketplace.database.product;
 
 import kuznetsov.marketplace.domain.product.Product;
+import kuznetsov.marketplace.domain.user.Customer;
+import kuznetsov.marketplace.domain.user.Seller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  Page<Product> findAllBySellerAndPreorderInfo_IdNotNull(Seller seller, Pageable page);
+
+  @Query("SELECT product FROM Product product"
+      + " INNER JOIN PreorderInfo preorderInfo"
+      + " ON product.preorderInfo.id = preorderInfo.id"
+      + " INNER JOIN PreorderParticipation participation"
+      + " ON preorderInfo.id = participation.preorderInfo.id"
+      + " WHERE participation.customer = :customer")
+  Page<Product> findAllByCustomer(Customer customer, Pageable page);
 
 }
