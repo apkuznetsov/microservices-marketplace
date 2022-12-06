241e6300e143f11d1b16887e675bd0fe108e3252
Andrey <ethenaut@ya.ru>
2022-12-06 20:31:54 +0400

    fix customer repo bug

--- src/main/java/kuznetsov/marketplace/database/user/CustomerRepository.java
+++ src/main/java/kuznetsov/marketplace/database/user/CustomerRepository.java
@@ -2,7 +2,6 @@ package kuznetsov.marketplace.database.user;
 
 import java.util.Optional;
 import kuznetsov.marketplace.domain.user.Customer;
-import kuznetsov.marketplace.domain.user.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,6 +10,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
   @Query("SELECT customer FROM Customer customer "
       + "INNER JOIN User user ON customer.user.id = user.id "
       + "WHERE user.email =: email AND user.role = 'CUSTOMER'")
-  Optional<Seller> findByUserEmail(String email);
+  Optional<Customer> findByUserEmail(String email);
 
 }
