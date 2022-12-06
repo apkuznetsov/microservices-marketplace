4f8ee1ae1b8d45b8af78b1c380738649acdc6e50
Andrey <ethenaut@ya.ru>
2022-12-06 20:25:12 +0400

    def user info dto

---
+++ src/main/java/kuznetsov/marketplace/services/user/dto/UserInfoDto.java
@@ -0,0 +1,21 @@
+package kuznetsov.marketplace.services.user.dto;
+
+import kuznetsov.marketplace.domain.user.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDto {
+
+  private Long id;
+
+  private String email;
+
+  private UserRole role;
+
+}
