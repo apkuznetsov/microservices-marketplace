78d727ca9b566d35153f3675a6dd75b2daf03f95
Andrey <ethenaut@ya.ru>
2023-03-25 07:20:21 +0400

    def chat dto

---
+++ marketplace-chat-backend/src/main/java/marketplace/chat/dto/ChatDto.java
@@ -0,0 +1,25 @@
+package marketplace.chat.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChatDto {
+
+    String id;
+
+    String name;
+
+    Instant createdAt;
+
+}
