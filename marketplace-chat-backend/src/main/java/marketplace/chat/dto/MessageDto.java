8a84b34581073831c746d859f425d9bc039f6a1d
Andrey <ethenaut@ya.ru>
2023-03-29 05:27:23 +0400

    def message dto

---
+++ marketplace-chat-backend/src/main/java/marketplace/chat/dto/MessageDto.java
@@ -0,0 +1,26 @@
+package marketplace.chat.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MessageDto {
+
+    String from;
+
+    String message;
+
+    @Builder.Default
+    Instant createdAt = Instant.now();
+
+}
