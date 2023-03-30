3ca5f039e3d35c02f7f2056d10697cce8d545bd7
Andrey <ethenaut@ya.ru>
2023-03-30 05:15:27 +0400

    def participant dto

---
+++ marketplace-chat-backend/src/main/java/marketplace/chat/dto/ParticipantDto.java
@@ -0,0 +1,23 @@
+package marketplace.chat.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ParticipantDto {
+
+    String id;
+
+    Instant enterAt;
+
+}
