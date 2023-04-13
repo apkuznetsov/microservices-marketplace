718346241ac5dcb1dde45e87aebd974e47a09e00
Andrey <ethenaut@ya.ru>
2023-04-13 04:48:47 +0400

    fix exception names

--- marketplace-chat/src/main/kotlin/kuznetsov/marketplace/chat/service/exception/InvalidUsernameException.kt
+++
@@ -1,3 +0,0 @@
-package kuznetsov.marketplace.chat.service.exception
-
-class InvalidUsernameException(message: String) : Exception(message)
--- marketplace-chat/src/main/kotlin/kuznetsov/marketplace/chat/service/exception/NotAuthorizedUserException.kt
+++
@@ -1,3 +0,0 @@
-package kuznetsov.marketplace.chat.service.exception
-
-class NotAuthorizedUserException(message: String) : Exception(message)
