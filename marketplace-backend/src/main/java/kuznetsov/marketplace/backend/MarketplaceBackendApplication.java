767e9529808612e106766219c6fd4e7fcdec620f
Andrey <ethenaut@ya.ru>
2023-05-06 08:30:21 +0400

    enable refresh scope

--- marketplace-backend/src/main/java/kuznetsov/marketplace/backend/MarketplaceBackendApplication.java
+++ marketplace-backend/src/main/java/kuznetsov/marketplace/backend/MarketplaceBackendApplication.java
@@ -2,8 +2,10 @@ package kuznetsov.marketplace.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class MarketplaceBackendApplication {
 
     public static void main(String[] args) {
