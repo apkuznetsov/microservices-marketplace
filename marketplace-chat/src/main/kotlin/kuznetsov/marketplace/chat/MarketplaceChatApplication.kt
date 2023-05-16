593cbb1146150e8720e9f6f9a2d965a670f70fd3
Andrey Kuznetsov <ethenaut@ya.ru>
2023-05-16 05:32:12 +0400

    config chat

--- marketplace-chat/src/main/kotlin/kuznetsov/marketplace/chat/MarketplaceChatApplication.kt
+++ marketplace-chat/src/main/kotlin/kuznetsov/marketplace/chat/MarketplaceChatApplication.kt
@@ -2,10 +2,12 @@ package kuznetsov.marketplace.chat
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 
 @SpringBootApplication
+@RefreshScope
 class MarketplaceChatApplication
 
 fun main(args: Array<String>) {
-	runApplication<MarketplaceChatApplication>(*args)
+    runApplication<MarketplaceChatApplication>(*args)
 }
