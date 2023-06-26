b4793e7410eca354df29a1692ebf7b53bcc9808d
Andrey Kuznetsov <ethenaut@ya.ru>
2023-06-26 05:12:09 +0400

    refactor web cors config

--- marketplace-backend/src/main/java/kuznetsov/marketplace/backend/web/WebCorsConfiguration.java
+++ marketplace-backend/src/main/java/kuznetsov/marketplace/backend/web/WebCorsConfiguration.java
@@ -11,23 +11,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.Collections;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebCorsConfiguration implements WebMvcConfigurer {
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Collections.singletonList(("*")));
-        configuration.setAllowedHeaders(Collections.singletonList(("*")));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Collections.singletonList("*"));
+        corsConfig.setAllowedMethods(Collections.singletonList(("*")));
+        corsConfig.setAllowedHeaders(Collections.singletonList(("*")));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource urlCorsConfig = new UrlBasedCorsConfigurationSource();
+        urlCorsConfig.registerCorsConfiguration("/**", corsConfig);
+
+        return urlCorsConfig;
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+    public void addCorsMappings(CorsRegistry config) {
+        config.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
