76416806e3a4819df78ba18ad1ee2b26c5287c15
Andrey Kuznetsov <ethenaut@ya.ru>
2023-06-26 05:09:54 +0400

    fix mistype

--- marketplace-eureka/src/main/java/kuznetsov/marketplace/eureka/MarketplaceEurekaApplication.java
+++ marketplace-eureka/src/main/java/kuznetsov/marketplace/eureka/MarketplaceEurekaApplication.java
@@ -1,4 +1,4 @@
-package kuznetosov.marketplace.eureka;
+package kuznetsov.marketplace.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
