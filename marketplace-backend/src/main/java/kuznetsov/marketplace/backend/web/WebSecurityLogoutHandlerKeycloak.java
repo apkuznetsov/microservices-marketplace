ef0df819b5da6cee2ea5d7f76981ca6b9a72a63e
Andrey Kuznetsov <ethenaut@ya.ru>
2023-06-30 05:27:00 +0400

    impl logout handler

---
+++ marketplace-backend/src/main/java/kuznetsov/marketplace/backend/web/WebSecurityLogoutHandlerKeycloak.java
@@ -0,0 +1,42 @@
+package kuznetsov.marketplace.backend.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSecurityLogoutHandlerKeycloak implements LogoutHandler {
+
+    private final RestTemplate restTemplate;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        logoutFromKeycloak((OidcUser) authentication.getPrincipal());
+    }
+
+    private void logoutFromKeycloak(OidcUser user) {
+        String endSessionEndpoint = user.getIssuer() + "/protocol/openid-connect/logout";
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(endSessionEndpoint)
+                .queryParam("id_token_hint", user.getIdToken().getTokenValue());
+
+        ResponseEntity<String> logoutResponse = restTemplate.getForEntity(
+                builder.toUriString(), String.class);
+        if (logoutResponse.getStatusCode().is2xxSuccessful()) {
+            log.info("Successfully logged out from Keycloak");
+        } else {
+            log.error("Could not propagate logout to Keycloak");
+        }
+    }
+
+}
