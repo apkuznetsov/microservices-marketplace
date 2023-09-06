4be534b6fced2d2c66d7485e397bbed59216928a
Andrey <ethenaut@ya.ru>
2023-09-06 05:18:47 +0400

    impl customer service

--- marketplace-backend/src/main/java/kuznetsov/marketplace/backend/service/CustomerValidator.java
+++
@@ -1,9 +0,0 @@
-package kuznetsov.marketplace.backend.service;
-
-import kuznetsov.marketplace.backend.dto.CustomerDto;
-
-public interface CustomerValidator {
-
-    void validateOrThrow(CustomerProperties customerProps, CustomerDto customerDto);
-
-}
