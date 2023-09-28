ce00e167da115bc42b60d5f8489ac780507f4388
Andrey Kuznetsov <ethenaut@ya.ru>
2023-09-28 05:12:05 +0400

    impl auth provider username password

---
+++ marketplace-auth/src/main/java/kuznetsov/marketplace/auth/service/AuthProviderUsernamePassword.java
@@ -0,0 +1,31 @@
+package kuznetsov.marketplace.auth.service;
+
+import kuznetsov.marketplace.auth.domain.AuthUsernamePassword;
+import kuznetsov.marketplace.auth.proxy.AuthServerProxy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthProviderUsernamePassword implements AuthenticationProvider {
+
+    private final AuthServerProxy proxy;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+        proxy.sendAuth(username, password);
+
+        return new UsernamePasswordAuthenticationToken(username, password);
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return AuthUsernamePassword.class.isAssignableFrom(aClass);
+    }
+}
