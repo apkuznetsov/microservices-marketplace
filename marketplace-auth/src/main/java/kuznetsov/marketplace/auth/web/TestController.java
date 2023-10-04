54d9e14d88c21972a4b9f40df8263846b70cd52d
Andrey Kuznetsov <ethenaut@ya.ru>
2023-10-04 07:04:06 +0400

    test controller

---
+++ marketplace-auth/src/main/java/kuznetsov/marketplace/auth/web/TestController.java
@@ -0,0 +1,14 @@
+package kuznetsov.marketplace.auth.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test";
+    }
+
+}
