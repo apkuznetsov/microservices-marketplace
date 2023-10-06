0747c610696bc71a5be0ff2d1d6d9fad5a8b9fd9
Andrey Kuznetsov <ethenaut@ya.ru>
2023-10-06 08:11:03 +0400

    oauth client

---
+++ marketplace-oauth-client/src/main/java/kuznetsov/marketplace/oauthclient/MarketplaceOauthClientApp.java
@@ -0,0 +1,13 @@
+package kuznetsov.marketplace.oauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceOauthClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceOauthClientApp.class, args);
+    }
+
+}
