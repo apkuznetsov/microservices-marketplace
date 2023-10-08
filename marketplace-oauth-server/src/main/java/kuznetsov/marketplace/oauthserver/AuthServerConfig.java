6ee77224275f2c95e53db10b2bd4aa11b3e70449
Andrey Kuznetsov <ethenaut@ya.ru>
2023-10-08 11:52:03 +0400

    oauth server

---
+++ marketplace-oauth-server/src/main/java/kuznetsov/marketplace/oauthserver/AuthServerConfig.java
@@ -0,0 +1,41 @@
+package kuznetsov.marketplace.oauthserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+@RequiredArgsConstructor
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client1")
+                .secret("secret1")
+                .authorizedGrantTypes("authorization_code")
+                .scopes("read")
+                .redirectUris("http://localhost:9090/home")
+
+                .and()
+                .withClient("client2")
+                .secret("secret2")
+                .authorizedGrantTypes(
+                        "authorization_code", "password", "refresh_token")
+                .scopes("read")
+                .redirectUris("http://localhost:9090/home");
+    }
+
+}
