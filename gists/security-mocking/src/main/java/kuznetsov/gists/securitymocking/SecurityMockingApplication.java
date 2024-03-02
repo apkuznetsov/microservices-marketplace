5f6ed86830f98d521ed01ddc1cb7bdf2f4a891f5
Андрей Кузнецов <ethenaut@ya.ru>
2024-03-02 18:03:31 +0400

    gradle project

---
+++ gists/security-mocking/src/main/java/kuznetsov/gists/securitymocking/SecurityMockingApplication.java
@@ -0,0 +1,13 @@
+package kuznetsov.gists.securitymocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMockingApplication.class, args);
+	}
+
+}
---
+++ gists/security-mocking/src/test/java/kuznetsov/gists/securitymocking/SecurityMockingApplicationTests.java
@@ -0,0 +1,13 @@
+package kuznetsov.gists.securitymocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
