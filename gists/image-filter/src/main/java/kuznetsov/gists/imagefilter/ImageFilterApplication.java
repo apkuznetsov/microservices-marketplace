998c9f5c859fb5c25cf12a0f406918d5b4e36b68
Андрей Кузнецов <ethenaut@ya.ru>
2024-03-07 21:23:32 +0400

    create template

---
+++ gists/image-filter/src/main/java/kuznetsov/gists/imagefilter/ImageFilterApplication.java
@@ -0,0 +1,13 @@
+package kuznetsov.gists.imagefilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageFilterApplication.class, args);
+    }
+
+}
