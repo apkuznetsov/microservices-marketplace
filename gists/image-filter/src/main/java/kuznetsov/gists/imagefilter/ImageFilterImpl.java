38aa5f40f2236a7dafb8a1172f7c427b7fac8b6c
Андрей Кузнецов <ethenaut@ya.ru>
2024-03-12 20:13:31 +0400

    convert to bmp

--- gists/image-filter/src/main/java/kuznetsov/gists/imagefilter/ImageFilterImpl.java
+++ gists/image-filter/src/main/java/kuznetsov/gists/imagefilter/ImageFilterImpl.java
@@ -6,6 +6,12 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
 @Component
@@ -16,6 +22,23 @@ public class ImageFilterImpl implements ImageFilter {
     @Override
     public MultipartFile filterUsingBmp(MultipartFile inputFile) {
         return null;
+    }
+
+    private @NotNull ByteArrayOutputStream toBmp(@NotNull InputStream inStream) throws IOException {
+        BufferedImage readImage = ImageIO.read(inStream);
+        BufferedImage newImage = new BufferedImage(
+                readImage.getWidth(),
+                readImage.getHeight(),
+                BufferedImage.TYPE_INT_RGB
+        );
+
+        Graphics2D g = newImage.createGraphics();
+        g.drawImage(readImage, 0, 0, null);
+        g.dispose();
+
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        ImageIO.write(newImage, "bmp", outStream);
+        return outStream;
     }
 
     private @NotNull String extractFormat(@NotNull MultipartFile mfile) {
