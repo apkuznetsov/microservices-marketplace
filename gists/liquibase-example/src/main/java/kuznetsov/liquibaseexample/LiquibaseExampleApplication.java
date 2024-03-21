a7d14c26f07d7f49c39bcd08d03a9c8565ccea68
Андрей Кузнецов <ethenaut@ya.ru>
2024-03-21 21:08:24 +0400

    create template

---
+++ gists/liquibase-example/src/main/java/kuznetsov/liquibaseexample/LiquibaseExampleApplication.java
@@ -0,0 +1,13 @@
+package kuznetsov.liquibaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseExampleApplication.class, args);
+    }
+
+}
---
+++ gists/liquibase-example/src/test/java/kuznetsov/liquibaseexample/LiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kuznetsov.liquibaseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
