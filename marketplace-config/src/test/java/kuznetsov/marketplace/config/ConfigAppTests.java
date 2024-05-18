d1d0e3580155b64a83ee5d693e3ebc0699f8a520
Андрей Кузнецов <ethenaut@ya.ru>
2024-05-18 10:24:32 +0400

    create template

---
+++ marketplace-config/src/test/java/kuznetsov/marketplace/config/ConfigAppTests.java
@@ -0,0 +1,13 @@
+package kuznetsov.marketplace.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
