a8f4fcf29acac79d7a141a3dd4102bbd44f2638a
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-04-19 17:27:08 +0400

    config data audit

---
+++ marketplace-core/src/main/java/kuznetsov/marketplace/core/data/DataConfiguration.java
@@ -0,0 +1,24 @@
+package kuznetsov.marketplace.core.data;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    AuditorAware<String> auditorAware() {
+        return () -> Optional
+                .ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName);
+    }
+
+}
