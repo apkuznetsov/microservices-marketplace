8ee3e819afea18b9a34ca2c48ada4a69c0457bd7
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-04-19 17:26:41 +0400

    fix security config

--- marketplace-core/src/main/java/kuznetsov/marketplace/core/web/SecurityConfiguration.java
+++ marketplace-core/src/main/java/kuznetsov/marketplace/core/web/SecurityConfiguration.java
@@ -1,25 +1,24 @@
 package kuznetsov.marketplace.core.web;
 
-import kuznetsov.marketplace.core.auth.service.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final JwtFilter jwtFilter;
+public class SecurityConfiguration {
 
     @Bean
     public InitializingBean initializingBean() {
@@ -32,15 +31,24 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf().disable()
-                .cors().disable()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .authorizeHttpRequests(req -> req
-                        // TODO
-                        .anyRequest().permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs",
+                                "/swagger-resources/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/index.html",
+                                "/swagger-ui/**",
+                                "/actuator/**",
+                                "/connect/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
                 )
-                .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
