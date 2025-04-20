40a478b3aad2c0a9bacbd0dfeacd92e1e42e0556
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-04-20 15:11:24 +0400

    config liquibase for starter

--- marketplace-settings-starter/src/main/java/kuznetsov/marketplace/settings/autoconfig/SettingStarterAutoconfig.java
+++ marketplace-settings-starter/src/main/java/kuznetsov/marketplace/settings/autoconfig/SettingStarterAutoconfig.java
@@ -1,12 +1,26 @@
 package kuznetsov.marketplace.settings.autoconfig;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import javax.sql.DataSource;
 
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
 @Import(SettingEntityRegistrar.class)
 public class SettingStarterAutoconfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db.changelog/db.changelog-settings-starter.xml");
+
+        return liquibase;
+    }
+
 }
