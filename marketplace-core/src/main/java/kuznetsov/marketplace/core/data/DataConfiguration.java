7237d9f1e1ad6f1ba3b53770b1bf418caac34356
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-04-20 18:47:34 +0400

    config core to work with second liquibase from lib

--- marketplace-core/src/main/java/kuznetsov/marketplace/core/data/DataConfiguration.java
+++ marketplace-core/src/main/java/kuznetsov/marketplace/core/data/DataConfiguration.java
@@ -1,5 +1,10 @@
 package kuznetsov.marketplace.core.data;
 
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -7,10 +12,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import javax.sql.DataSource;
 import java.util.Optional;
 
 @Configuration
+@EnableConfigurationProperties(LiquibaseProperties.class)
 public class DataConfiguration {
+
+    @Bean
+    @SneakyThrows
+    public SpringLiquibase springLiquibase(
+            @Value("${spring.liquibase.change-log}") String changeLog,
+            DataSource dataSource
+    ) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setLiquibaseSchema(dataSource.getConnection().getSchema());
+        liquibase.setChangeLog(changeLog);
+
+        return liquibase;
+    }
 
     @Bean
     AuditorAware<String> auditorAware() {
