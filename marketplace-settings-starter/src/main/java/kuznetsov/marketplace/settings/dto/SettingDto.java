f7e03c3600ab74da8c613d70aa591f6c84906868
Andrey Kuznetsov <a.p.kuznetsov@vk.com>
2025-04-20 19:03:37 +0400

    fix deserialization error

--- marketplace-settings-starter/src/main/java/kuznetsov/marketplace/settings/dto/SettingDto.java
+++ marketplace-settings-starter/src/main/java/kuznetsov/marketplace/settings/dto/SettingDto.java
@@ -2,11 +2,13 @@ package kuznetsov.marketplace.settings.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 public class SettingDto {
 
     private String code;
